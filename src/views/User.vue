f0c70aff0f9e70aaf9af2d089a9cdb7abfa9be1c
RachelChen <iceage91@gmail.com>
2021-12-06 20:37:22 +0800

    add followship-likeship api

--- src/views/User.vue
+++ src/views/User.vue
@@ -88,6 +88,11 @@ export default {
       getCurrentUser: GET_CURRENT_USER,
     }),
   },
+  computed: {
+    ...mapGetters({
+      getCurrentUser: GET_CURRENT_USER,
+    }),
+  },
   // created() {
   //   this.setTweetsFilterType({
   //     getter: GET_ONE_USER_TWEETS,
